76cbf7899aef66495978d9c722b01d1fc3f04325
Rahul <129066752+Rahulseenu02@users.noreply.github.com>
2023-04-09 16:51:47 +0530

    innitial first

---
+++ Pro04.java
@@ -0,0 +1,22 @@
+class Pro04 
+{
+static void add(int a,int b)
+	{
+ int c= a+b;
+ System.out.println("sum = " + c);
+ return;
+
+}
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**************");
+		add(10,20);
+		add(250,255);
+		System.out.println("**************");
+
+
+
+	}
+}
